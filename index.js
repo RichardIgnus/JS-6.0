b5dfeccd6ae302d9fd88947a37261aece032ab6a
RichardIgnus <rytis.bukauskas1@gmail.com>
2021-11-08 22:23:56 +0200

    Added comments to indicate which part it is

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 6.1
 let people = [
     {
         id: 1,
@@ -21,12 +22,14 @@ let people = [
     },
 ];
 
+// 6.2
 const filteredArray = people.filter((person) => person.isMale !== true);
 const mappedArray =  people.map(person => ({id: person.id, firstName: person.name, isFemale: person.isMale !== true}));
 console.log('-- Sukurti masyvą (Array) --', '\n', people);
 console.log('-- Išfiltruoti masyvą pagal parametra isMale --', '\n', filteredArray);
 console.log(' -- Panaudoti map ir sukurti naują masyvą --', '\n', mappedArray);
 
+//6.3
 const numbers = [ 1, -2, 3, -4, 5];
 
 const filteredArray2 = numbers.filter(number => number >= 0);
